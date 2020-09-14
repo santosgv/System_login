82130e08ae13cf01af06171e50026937fdb58ef3
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2020-09-14 12:01:55 -0300

    Update Main.py
    class validadores

--- Main.py
+++ Main.py
@@ -1,3 +1,44 @@
-from Check_list.db import mycursor
-from Check_list.Class import Aplicacao
+
+from tkinter import *
+
+root=Tk()
+
+class Validadores():
+    def validar(self):
+        login=self.entlog.get()
+        senha=self.entpwd.get()
+        self.db()
+        return print(login,senha)
+
+    def db(self):
+        return print('Acessando banco')
+class Aplicacao(Validadores):
+    def __init__(self):
+        self.root=root
+        self.Tela()
+        self.Frame()
+        self.labelAndBotton()
+        root.mainloop()
+    def Tela(self):
+        self.root.title('System_login')
+        self.root.configure(background='black')
+        self.root.geometry('300x200')
+        self.root.maxsize(width=1280,height=1024)
+    def Frame(self):
+        self.main=Frame(self.root,bg='white')
+        self.main.place(relx=0.01,rely=0.01,relwidth=0.98,relheight=0.98)
+    def labelAndBotton(self):
+        self.lblog=Label(self.main,bg='white',text='Login')
+        self.lblog.place(relx=0.05,rely=0.15)
+        self.entlog=Entry(self.main)
+        self.entlog.place(relx=0.20,rely=0.15,width=150)
+
+        self.lbpwd=Label(self.main,bg='white',text='Senha')
+        self.lbpwd.place(relx=0.05,rely=0.45)
+        self.entpwd=Entry(self.main)
+        self.entpwd.place(relx=0.20,rely=0.45,width=150)
+
+        self.btlog=Button(self.main,text='Logar',command=self.validar)
+        self.btlog.place(relx=0.3,rely=0.60)
+
 Aplicacao()
