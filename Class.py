244b728fc31c99db52e2db3598872040c6bc9d6b
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2020-09-14 12:01:39 -0300

    Update Class.py
    removido

--- Class.py
+++ Class.py
@@ -1,16 +1,1 @@
-from tkinter import ttk
-from tkinter import *
-
-root=Tk()
-class Aplicacao():
-    def __init__(self):
-        self.root=root
-        self.Tela()
-        root.mainloop()
-    def Tela(self):
-        self.root.title('Gerenciador')
-        self.root.configure(background='black')
-        self.root.geometry('800x600')
-        self.root.maxsize(width=1280,height=1024)
-        self.root.minsize(width=800,height=600)
-Aplicacao()
+from Check_list.Main import Aplicacao
