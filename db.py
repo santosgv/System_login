f30e413e77587192f295c9fadb494fcbf5b1f0be
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2020-09-15 13:07:39 -0300

    Update db.py
    Correct

--- db.py
+++ db.py
@@ -1,3 +1,16 @@
 from mysql.connector import (connection)
 mydb = connection.MySQLConnection(host='127.0.0.1', user='root', password='Senha123@', database='Fist_db')
 mycursor=mydb.cursor()
+
+def checklogin(login,passwoard):
+    mycursor.execute(f'''
+    select * from Login where user=('{login}') and password=('{passwoard}')
+    ''')
+    result = mycursor.fetchall()
+    if len(result)==1:
+        result=True
+
+    elif len(result)==0:
+        result=False
+
+    return print(result)
