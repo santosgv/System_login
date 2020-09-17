4649265b4bd83a05961ebcedd6ce2087d7ae12d7
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2020-09-17 11:12:28 -0300

    new function

--- db.py
+++ db.py
@@ -3,9 +3,9 @@ mydb = connection.MySQLConnection(host='127.0.0.1', user='root', password='Senha
 mycursor=mydb.cursor()
 
 
-def checklogin(login,passwoard):
+def checklogin(login,password):
     mycursor.execute(f'''
-        select * from Login where user=('{login}') and password=('{passwoard}')
+        select * from Login where user=('{login}') and password=('{password}')
         ''')
     status = mycursor.fetchall()
     if len(status)==1:
@@ -18,3 +18,5 @@ def checklogin(login,passwoard):
         status='Foi encontrado mas de um resultado para o usuario'
 
     return status
+def creatlogin():
+    return print('deu certo')
