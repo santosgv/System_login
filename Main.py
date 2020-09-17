746977be17f6ec0a2f003ba17c73324885348e4e
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2020-09-17 11:01:57 -0300

    new function

--- Main.py
+++ Main.py
@@ -7,10 +7,10 @@ class Funcoes():
     def validar(self):
         status=checklogin(self.entlog.get(),self.entpwd.get())
         if status ==True:
-            self.lbtext['text'] = ('Login acept')
+            self.lbtext['text']=('Login acept')
             self.lbtext.place(relx=0.2, rely=0.80)
         elif status ==False:
-            self.lbtext['text'] = ('Login or password incorrect')
+            self.lbtext['text']= ('Login or password incorrect')
             self.lbtext.place(relx=0.1, rely=0.80)
         else:
             self.lbtext['text'] = (status)
@@ -42,8 +42,23 @@ class Aplicacao(Funcoes):
         self.entpwd.place(relx=0.20,rely=0.45,width=150)
 
         self.btlog=Button(self.main,text='Logar',command=self.validar)
-        self.btlog.place(relx=0.3,rely=0.60)
+        self.btlog.place(relx=0.5,rely=0.60)
 
-        self.lbtext=Label(self.main,text='Status',bg='white')
+        self.btacc=Button(self.main,text='New',command=self.Newwindow)
+        self.btacc.place(relx=0.10,rely=0.60)
+
+        self.lbtext=Label(self.main,text='Situation',bg='white')
         self.lbtext.place(relx=0.2,rely=0.80)
+    def Newwindow(self):
+        nwwindow=Toplevel(self.root,bg='white')
+        nwwindow.geometry('250x100')
+        nwwindow.title('Create New Accont')
+        nwwindow.resizable(False,False)
+        Label(nwwindow,text='Login',bg='white').place(relx=0.01,rely=0.20)
+        Label(nwwindow, text='Password',bg='white').place(relx=0.01, rely=0.40)
+        login=Entry(nwwindow).place(relx=0.25,rely=0.20)
+        pasword=Entry(nwwindow).place(relx=0.25, rely=0.40)
+        Button(nwwindow,text='Creat').place(relx=0.4,rely=0.70)
+
+
 Aplicacao()
